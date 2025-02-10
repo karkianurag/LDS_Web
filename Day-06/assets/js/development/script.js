075ffdd0e2126d6d419523e1855f385adf09abea
karkianurag <anurag@lastdoor.co>
2025-02-10 09:40:52 +0545

    Day-6 added

--- Day-06/assets/js/development/script.js
+++ Day-06/assets/js/development/script.js
@@ -1,23 +1,21 @@
+// const progressBars = document.querySelectorAll(".hero__progress-bar");
+
 const swiper1 = new Swiper(".myswiper", {
-  loop: true,
+  loop: false,
 
   slidesPerView: 3,
   spaceBetween: 20,
   freeMode: true,
-  watchSlidePreogress: true,
+  watchSlideProgress: true,
 
   // navigation: {
   //   nextEl: ".swiper-button-next",
   //   prevEl: ".swiper-button-prev",
   // },
-  // autoplay: {
-  //   delay: 2500,
-  //   disableOnInteraction: false,
-  // },
 });
 
 const swiper2 = new Swiper(".myswiper2", {
-  loop: true,
+  loop: false,
 
   slidesPerView: 1,
   spaceBetween: 20,
@@ -26,23 +24,57 @@ const swiper2 = new Swiper(".myswiper2", {
   //   prevEl: ".swiper-button-prev",
   // },
   autoplay: {
-    delay: 2500,
+    delay: 2900,
     disableOnInteraction: false,
   },
 
   thumbs: {
     swiper: swiper1,
   },
+  on: {
+    slideChange: function () {
+      updateProgressBar(this);
+    },
+    slideProgress: function () {
+      updateProgressBar(this);
+    },
+  },
 });
 
-const swiper3 = new Swiper(".swiper-container", {
-  loop: true,
+window.addEventListener("load", () => {
+  updateProgressBar(swiper2); // Update progress bar on page load
+});
 
-  slidesPerView: 3,
+//
+function updateProgressBar(swiperInstance) {
+  const progressBars = document.querySelectorAll(".hero__progress-bar-fill");
+
+  progressBars.forEach((progressBar, index) => {
+    let progress = (swiperInstance.progress * 100).toFixed(2);
+
+    // Check if the progress bar is active
+    if (index === swiperInstance.activeIndex) {
+      progressBar.style.animation = "fillProgressBar 3s linear forwards";
+      progressBar.style.width = `${progress}%`;
+    } else {
+      progressBar.style.animation = "none";
+      progressBar.style.width = "0%";
+    }
+  });
+}
+
+const swiper3 = new Swiper(".swiper-container", {
+  loop: false,
+
+  slidesPerView: 1,
   spaceBetween: 20,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    768: { slidesPerView: 2, spaceBetween: 20 },
+    1024: { slidesPerView: 4, spaceBetween: 20 },
   },
   // pagination: {
   //   el: ".swiper-pagination",
