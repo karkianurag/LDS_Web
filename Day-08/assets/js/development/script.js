642b0cdf3c5fe37eafbf13bfb3cfe1a44e4214dc
karkianurag <anurag@lastdoor.co>
2025-02-13 16:37:20 +0545

    changes made to design if the containers except hero container

--- Day-08/assets/js/development/script.js
+++ Day-08/assets/js/development/script.js
@@ -1,7 +1,13 @@
-// const progressBars = document.querySelectorAll(".hero__progress-bar");
+const totalSlides1 = document
+  .querySelector(".myswiper")
+  .querySelectorAll(".swiper-slide").length;
 
+const totalSlides2 = document
+  .querySelector(".swiper-container")
+  .querySelectorAll(".swiper-slide").length;
+console.log(totalSlides2);
 const swiper1 = new Swiper(".myswiper", {
-  loop: true,
+  loop: false,
 
   slidesPerView: 3,
   spaceBetween: 20,
@@ -23,10 +29,10 @@ const swiper2 = new Swiper(".myswiper2", {
   //   nextEl: ".swiper-button-next",
   //   prevEl: ".swiper-button-prev",
   // },
-  autoplay: {
-    delay: 2900,
-    disableOnInteraction: false,
-  },
+  // autoplay: {
+  //   delay: 2900,
+  //   disableOnInteraction: false,
+  // },
 
   thumbs: {
     swiper: swiper1,
@@ -64,7 +70,7 @@ function updateProgressBar(swiperInstance) {
 }
 
 const swiper3 = new Swiper(".swiper-container", {
-  loop: true,
+  loop: totalSlides2 > 1,
 
   slidesPerView: 1,
   spaceBetween: 20,
@@ -73,8 +79,20 @@ const swiper3 = new Swiper(".swiper-container", {
     prevEl: ".swiper-button-prev",
   },
   breakpoints: {
-    768: { slidesPerView: 2, spaceBetween: 20 },
-    1024: { slidesPerView: 4, spaceBetween: 20 },
+    480: {
+      loop: totalSlides2 > 1,
+      slidesPerView: 2,
+    },
+    // 768: {
+    //   loop: totalSlides2 > 2,
+    //   slidesPerView: 2,
+    //   spaceBetween: 20,
+    // },
+    1024: {
+      loop: totalSlides2 > 4,
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
   },
   // pagination: {
   //   el: ".swiper-pagination",
@@ -83,6 +101,11 @@ const swiper3 = new Swiper(".swiper-container", {
 });
 
 Fancybox.bind('[data-fancybox="gallery"]', {
+  // Options for Fancybox (if needed)
+  infinite: true, // Allow cycling through content in the lightbox
+});
+
+Fancybox.bind('[data-fancybox="success__gallery"]', {
   // Options for Fancybox (if needed)
   infinite: true, // Allow cycling through content in the lightbox
 });
